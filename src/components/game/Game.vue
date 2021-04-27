3dacd73a5bc4371248ccafbb48d9e978baac95fc
Dominic Ojeda <dominicojeda.coding@gmail.com>
2021-04-26 21:57:46 -0600

    loading game takes you out of save menu.

--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -238,6 +238,7 @@ export default {
             this.location = 'Town'
             this.resetMerchant()
             this.resetQuestBoard()
+            this.$parent.menu = false;
             this.$parent.gameStarted = true;
             console.log('P1:',this.player)
             console.log('R:',this.region)
